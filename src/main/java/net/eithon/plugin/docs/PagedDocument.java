af7565a11760f4eeb104168db431362c75d6bcbd
vanbrayne <lars.lindberg@xlent.se>
2015-04-29 20:01:26 +0200

    Now uses the new library chat functionality.

--- src/main/java/net/eithon/plugin/docs/PagedDocument.java
+++ src/main/java/net/eithon/plugin/docs/PagedDocument.java
@@ -7,27 +7,26 @@ import net.eithon.library.chat.Paginator;
 import net.eithon.library.chat.SimpleMarkUp;
 
 class PagedDocument {
-	private SimpleMarkUp _parsedFile = null;
-	private Page[] _chatPages = null;
+	private Page[] _pages = null;
 	private File _file;
 	private int _widthInPixels = 320;
 
 	public PagedDocument(File file, int widthInPixels) {
 		this._file = file;
 		this._widthInPixels = widthInPixels;
-		this._parsedFile = new SimpleMarkUp(this._file);
 		reloadRules();
 	}
 
-	public int getNumberOfPages(){ return this._chatPages.length; }
+	public int getNumberOfPages(){ return this._pages.length; }
 
 	public String[] getPage(int pageNumber){
 		if ((pageNumber < 1) || (pageNumber > getNumberOfPages())) return null;
-		return this._chatPages[pageNumber-1].getLines();
+		return this._pages[pageNumber-1].getLines();
 	}
 
 	public void reloadRules() {
-		String[] allLines = this._parsedFile.getParsedLines();
-		this._chatPages = Paginator.paginate(allLines, this._widthInPixels);
+		SimpleMarkUp parsedFile = new SimpleMarkUp(this._file);
+		String[] allLines = parsedFile.getParsedLines();
+		this._pages = Paginator.paginate(allLines, this._widthInPixels);
 	}
 }
